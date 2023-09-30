582819f4a8b5123d24e0da0e0d7f477c4e60cd51
Rina-Matsumoto <ri0322@stu.kanazawa-u.ac.jp>
2023-09-30 13:42:11 +0900

    breezeの日本語化

--- app/Http/Controllers/User/Auth/RegisteredUserController.php
+++ app/Http/Controllers/User/Auth/RegisteredUserController.php
@@ -33,12 +33,14 @@ class RegisteredUserController extends Controller
         $request->validate([
             'name' => ['required', 'string', 'max:255'],
             'email' => ['required', 'string', 'email', 'max:255', 'unique:'.User::class],
+            'admin_id' => ['required'],
             'password' => ['required', 'confirmed', Rules\Password::defaults()],
         ]);
 
         $user = User::create([
             'name' => $request->name,
             'email' => $request->email,
+            'admin_id' => $request->admin_id,
             'password' => Hash::make($request->password),
         ]);
 
