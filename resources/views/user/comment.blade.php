c30d7d5596603fa43499e21bfc1d062a65e8db18
Rina-Matsumoto <ri0322@stu.kanazawa-u.ac.jp>
2023-10-02 23:04:42 +0900

    チャット機能の実装

--- resources/views/user/comment.blade.php
+++ resources/views/user/comment.blade.php
@@ -1,26 +1,32 @@
-@extends('layouts.comment')
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <title>ステータス</title>
 
-@section('content')
-<div class="chat-container row justify-content-center">
-    <div class="chat-area">
-        <div class="card">
-            <div class="card-header">Comment</div>
-            <div class="card-body chat-card">
-                @foreach ($comments as $item)
-                @include('components.comment', ['item' => $item])
-                @endforeach
-            </div>
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+        <script src="https://cdn.tailwindcss.com"></script>
+    </head>
+    <body class="w-4/5 md:w-3/5 lg:w-2/5 m-auto">
+        <h1 class="my-4 text-3xl font-bold">{{env('APP_NAME')}}</h1>
+            <ul>
+                
+                    @foreach ($comments as $comment)
+                        <p class="text-xs @if($comment->user_identifier === session('user_identifier')) text-right @endif">{{$comment->created_at}}</p>
+                        <li class="w-max mb-3 p-2 rounded-lg bg-blue-200 relative @if($comment->user_identifier === session('user_identifier')) self ml-auto @else other @endif">
+                            <div class="p-2 rounded-lg bg-blue-200">
+                                {{$comment->comment}}
+                            </div>
+                        </li>
+                    @endforeach
+            </ul>
         </div>
-    </div>
-</div>
-
-<form method="POST" action="">
-    @csrf
-    <div class="comment-container row justify-content-center">
-        <div class="input-group comment-area">
-            <textarea class="form-control" id="comment" name="comment" aria-label="With textarea"></textarea>
-            <button type="submit" id="submit" class="btn btn-outline-primary comment-btn">Submit</button>
-        </div>
-    </div>
-</form>
-@endsection
+        <form class="my-4 py-2 px-4 rounded-lg bg-gray-300 text-sm flex flex-col md:flex-row flex-grow" action="" method="POST">
+            @csrf
+            <input type="hidden" name="user_identifier" value={{session('user_identifier')}}>
+            <input class="mt-2 md:mt-0 md:ml-2 py-1 px-2 rounded flex-auto" type="text" name="comment" placeholder="Input comment." maxlength="200" required>
+            <button class="mt-2 md:mt-0 md:ml-2 py-1 px-2 rounded text-center bg-gray-500 text-white" type="submit">Send</button>
+        </form>
+    </body>
+</html>
